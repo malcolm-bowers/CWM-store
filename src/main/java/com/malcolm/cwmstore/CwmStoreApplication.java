0ca12df553eef5a025bc77f298654954a0385472
Malcolm Bowers <malcolm.bowers@icloud.com>
2025-07-14 11:50:05 -0500

    Initial push

---
+++ src/main/java/com/malcolm/cwmstore/CwmStoreApplication.java
@@ -0,0 +1,13 @@
+package com.malcolm.cwmstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwmStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwmStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/malcolm/cwmstore/CwmStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malcolm.cwmstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwmStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
